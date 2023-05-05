d79c9b8c505b5ff86e5959d8d84bbd666f5dcc1c
wxzhang <wxzhang@126.com>
2023-05-05 16:54:53 +0800

    refactor(core): add tests

--- packages/core/__tests__/index.test.ts
+++ packages/core/__tests__/index.test.ts
@@ -1,1 +1,14 @@
 import {test,expect} from "vitest"
+import { VoerkaLogger } from '../src/Logger';
+
+
+test("缓存输出",()=>{
+    let logger = new VoerkaLogger({
+        injectGlobal:false,
+        enabled:false
+    })
+    new Array(10).fill(0).forEach((_,i)=>{
+        logger.info(`test:${i+1}`)
+    })
+
+})
